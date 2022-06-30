30aa729d377cd9c9cbc0e807b0afe774fe0ee881
anetastankovska <anetastankovskaane@gmail.com>
2022-06-30 11:10:21 +0200

    Added code for the portfolio table

--- src/scripts/simulatorPage/simulatorPage.js
+++ src/scripts/simulatorPage/simulatorPage.js
@@ -8,6 +8,39 @@
 
 //-------------------------------------------------------------------------------------------------------
 //#region  Aneta Stankovska => TODO: Create Portfolio in Simulator
+let portfolioHelpers = {
+    "portfolioTable" : {}
+};
+function renderPortfolioTable() {
+    let counter = 1;
+    let strArr = [];
+    let userWallet = localStorage.getItem(User, JSON.parse(Wallet));
+    let userWalletCoins = userWallet.coins;
+    strArr.push(`<table id="dtBasicExample" class="table table-hover table-responsive table-fit">
+    <thead>
+      <tr>
+        <th scope="col" class="table-sm">#</th>
+        <th scope="col" class="table-sm">Name</th>
+        <th scope="col" class="table-sm">Amount</th>
+        <th scope="col" class="table-sm">Value</th>
+        <th scope="col" class="table-sm">Change in %</th>
+        <th scope="col" class="table-sm"></th>
+      </tr>
+    </thead>
+    <tbody>
+    `);
+
+    for (let coin of userWallet){
+      strArr.push(`<tr>${counter++}</tr>
+      <tr>${coin.name}</tr>
+      <tr>${coin.quantity}</tr>
+      <tr>${coin.currentPrice * coin.quantity}</tr>
+      <tr>${(coin.currentPrice - coin.priceBought)*100}</tr>
+      <tr id="sellCoin"><button>Sell</button></tr>`)
+    }
+};
+
+
 //#endregion
 
 //-------------------------------------------------------------------------------------------------------
